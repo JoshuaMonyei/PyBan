9332c7c6d0a7bb0f02498db5cd00a0a587791135
JoshuaMonyei <monyeijosh01@gmail.com>
2020-05-09 20:17:39 +0100

    Update BankingSystem.py

--- BankingSystem.py
+++ BankingSystem.py
@@ -1,10 +1,9 @@
 def root():
     import json
-    import os
-    login_page = input("""what would you like to do? 
+    login_page = input("""what would you like to do? Enter 1 or 2 to choose
                         1. Staff login 
                         2. Close App: """).lower()
-    if login_page == "staff login":
+    if login_page == "1":
         username = input("Please input your username: ")
         password = input("Please input your password: ")
         # opening staff.txt file to verify user input
@@ -15,11 +14,11 @@ def root():
                 username == (staff_details['Staff2']['username']) and password == (
                 staff_details['Staff2']['password']):
             print("Login Successful")
-            home_page()
+            staff_home_page()
         else:
             print("Incorrect username or password")
             root()
-    elif login_page == "close app":
+    elif login_page == "2":
         exit()
     else:
         print("Invalid Entry")
@@ -27,16 +26,16 @@ def root():
         root()
 
 
-import random
-
-
 # creating a method for staff options after a successful login
-def home_page():
-    staff_options = input("""What would you like to do?
+def staff_home_page():
+    staff_options = input("""What would you like to do? Enter 1, 2 0r 3 to choose
                          1. Create a new bank account
                          2. Check Account Details
                          3. Logout: """).lower()
-    if staff_options == "create new bank account":
+    if staff_options == "1":
+        import random
+        import json
+        import os
         acct_name = input("Account name: ")
         opening_bal = input("Opening Balance: ")
         acct_type = input("Account type: ")
@@ -44,31 +43,50 @@ def home_page():
         # generating random number to use as acct number
         acct_num = ("017" + random.randrange(0, 10 ** 7).__str__())
         print("Your account number is: " + acct_num)
-        # saving variables in the customer.txt file
-        file = open('customer.txt', 'a')
-        file.write("Account name: " + acct_name + " ")
-        file.write("Account Balance: " + opening_bal + "$ ")
-        file.write("Account type: " + acct_type + " ")
-        file.write("Account email: " + acct_email + " ")
-        file.write("Account number: " + acct_num + "\n")
-        file.close()
-        home_page()
-    elif staff_options == "check account details":
-        check_acct = input("Please what's your account number: ")
-        # reading out saved file from customer.txt file.
-        search = open("customer.txt", "r")
-        for lines in search:
-            if lines.__contains__(check_acct):
-                print(lines)
-        home_page()
-    elif staff_options == "logout":
+        # creating dictionaries to save in the customer.txt file
+        overall_data = {}
+        banking_details = {
+            'Account name': acct_name, 'Opening Balance': opening_bal + '$',
+            'Account type': acct_type, 'Account email': acct_email
+        }
+        overall_data[acct_num] = banking_details
+        # check if customer.txt is empty by doing this, the essence of this is to enable the smooth reading of text from the file.
+        if os.stat('customer.txt').st_size == 0:
+            with open('customer.txt', 'w') as f_obj:
+                json.dump(overall_data, f_obj)
+        else:
+            # i.e. if the customer.txt is not empty
+            with open('customer.txt') as f_obj:
+                # it will load the contents of customer.txt to the variable overall_data
+                overall_data = json.load(f_obj)
+                # Then append the present user details
+                overall_data[acct_num] = banking_details
+                # Then open the customer file in write mode and dump the overall_data to it
+                with open('customer.txt', 'w') as f:
+                    json.dump(overall_data, f)
+        staff_home_page()
+    elif staff_options == "2":
+        import json
+        check_acct = input('Enter account number to check: ')
+        # So to check it on the customer.txt file
+        with open('customer.txt') as file_obj:
+            data = json.load(file_obj)
+            if check_acct in data:
+                print('\nAccount Found ! See details below:')
+                for keys, values in data[check_acct].items():
+                    print('\n\t' + keys + ' : ' + str(values))
+                staff_home_page()
+            else:
+                print('Account does not exist! You can register a new one if you wish. Choose by entering 1 below')
+                staff_home_page()
+    elif staff_options == "3":
         print("Logout successful")
         # recalling the root method
         root()
     else:
         print("Invalid entry")
         # recalling the home page after an invalid entry among staff options
-        home_page()
+        staff_home_page()
 
 
 root()
